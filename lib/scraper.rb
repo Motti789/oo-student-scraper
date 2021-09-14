aafcb340cf2c05c7b269ebb78a30f8d99e1800f2
Motti789 <mchillywilly@gmail.com>
2021-09-14 10:33:04 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,19 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+        html = URI.open(index_url)
+        doc = Nokogiri::HTML(html)
+        students = []
+        data = doc.css(".roster-body-wrapper")
+        data.each do |student|
+          puts student.text.strip
+        end
     
   end
 
   def self.scrape_profile_page(profile_url)
+    html = URI.open(profile_url)
+    doc = Nokogiri::HTML(html)
     
   end
 
